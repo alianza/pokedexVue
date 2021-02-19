556817b6989fb65cf039dc6298b7b6973a8846a6
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-02-19 15:32:08 +0100

    Fied menu highlighting

--- src/components/PokeMenu.vue
+++ src/components/PokeMenu.vue
@@ -3,9 +3,9 @@
     <div v-on:click="handleMenuIconClick" class="menu-close">✖</div>
     <h1>Menu</h1>
     <ul class="menu-top">
-      <router-link tag="li" to="/" >Home</router-link>
-      <router-link tag="li" to="/types">Types</router-link>
-      <li v-on:click="handleMenuItemClick('random', $event.target);">Random Pokemon</li>
+      <router-link tag="li" to="/" :active-class="$route.path.includes('allpokemondetail') ? 'active' : ''">Home</router-link>
+      <router-link tag="li" to="/types" :active-class="$route.path.includes('type') ? 'active' : ''">Types</router-link>
+      <router-link tag="li" to="/random">Random Pokémon</router-link>
       <li v-on:click="handleMenuItemClick('about', $event.target);">About</li>
     </ul>
     <p class="menu-bottom">Jan-Willem van Bremen</p>
@@ -17,6 +17,10 @@ import Vue from "vue";
 
 export default Vue.extend({
   name: "PokeMenu",
+  updated() {
+    console.log(this.$route);
+    console.log(this.$route.path.includes('type'));
+  },
   methods: {
     handleMenuIconClick() {
       this.$emit('clickedMenuIcon');
@@ -28,6 +32,76 @@ export default Vue.extend({
 })
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.menu {
+  position: fixed;
+  width: 11em;
+  padding: 1em;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+  top: 0;
+  background-color: white;
+  height: 100%;
+  transition: transform .5s;
+  transform: translateX(-13em);
 
+  & h1 {
+    margin-top: .5em;
+  }
+
+  &-close {
+    display: none;
+    cursor: pointer;
+    transition: transform .1s;
+
+    &:hover {
+      transform: scale(1.2);
+    }
+
+    &:active {
+      transform: scale(.98);
+    }
+  }
+
+  &-top {
+
+  }
+
+  &-bottom {
+    bottom: 2em;
+    position: absolute;
+  }
+
+  & ul {
+    list-style-type: none;
+    padding: 0;
+
+    & li {
+      list-style: none;
+      margin: 1em;
+      transition: transform .2s;
+      cursor: pointer;
+
+      &.active {
+        font-weight: bold;
+      }
+
+      &:hover {
+        transform: scale(1.1);
+      }
+
+      &:active {
+        transform: scale(.98);
+      }
+    }
+  }
+}
+
+@media only screen and (max-width: 400px) {
+    .menu {
+
+      &-close {
+        display: inline-block;
+      }
+    }
+}
 </style>
