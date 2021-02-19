69ab91e52b66e47c8540430eb661b7eca4e27ee5
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-02-19 15:32:19 +0100

    Added routes

--- src/main.js
+++ src/main.js
@@ -18,9 +18,9 @@ const routes = [
   { path: '/', component: AllPokemons },
   { path: '/page/:page', component: AllPokemons },
   { path: '/types', component: Types },
-  { path: '/type/:type', component: TypePokemons },
-  { path: '/AllPokemonDetail/:pokemonName', name: 'AllPokemonDetail' ,components: { default: AllPokemons, detail: PokemonDetail } },
-  { path: '/type/:type/TypePokemonDetail/:pokemonName', name: 'TypePokemonDetail' ,components: { default: TypePokemons, detail: PokemonDetail } },
+  { path: '/types/:type', component: TypePokemons },
+  { path: '/allpokemondetail/:pokemonName', name: 'allpokemondetail' ,components: { default: AllPokemons, detail: PokemonDetail } },
+  { path: '/types/:type/typespokemondetail/:pokemonName', name: 'typespokemondetail' ,components: { default: TypePokemons, detail: PokemonDetail } },
 ];
 
 const router = new VueRouter({ routes, linkExactActiveClass: "active", mode: "history" });
